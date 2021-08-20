a7a1b3ea90d31628db0e8e1e54f6a44dd217a610
rnaz25 <rnaz25@gmail.com>
2021-08-20 11:46:57 +0530

    taking input and console

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-console.log("working from another file")
-var username = prompt("give me usersname")
-alert("this script works " + username)
+var btnTranslate = document.querySelector("#btn-translate")
+var txtInput = document.querySelector("#txt-input")
+
+console.log(txtInput)
+
+function clickHandler () {
+    console.log("clicked!");
+    console.log("input", txtInput.value);
+
+
+};
+btnTranslate.addEventListener("click", clickHandler)
