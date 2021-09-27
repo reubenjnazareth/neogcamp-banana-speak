d5a6683fde89e4aaec5da4045dbcd161d94cba91
rnaz25 <rnaz25@gmail.com>
2021-09-28 01:53:56 +0530

    minor update

--- apptrial.js
+++ apptrial.js
@@ -2,23 +2,19 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-function getTranslationURL(text) {
-    return serverURL + "?" + "text=" + text
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
 }
-
 
 function errorHandler(error) {
     console.log("error occured", error);
     alert("something wrong with server! try again after some time...")
 }
 
-
 function clickHandler() {
-    //outputDiv.innerText="bannananannana " + txtInput.value ;
+    
     var inputText = txtInput.value //taking input
 
     //calling server for processing
@@ -26,10 +22,9 @@ function clickHandler() {
       .then(response => response.json())
       .then(json => {
           var translatedText = json.contents.translated;
-          outputDiv.innerText = translatedText;
+          outputDiv.innerText = translatedText; //output
       })
       .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler)
-
